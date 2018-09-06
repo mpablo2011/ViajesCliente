6df1f295e6a2f7f2fc2a0cbdec279b890d130577
lucio.tzikas <lucio.tzikas@gmail.com>
2018-09-06 12:22:46 -0300

    ok

--- src/vistas/AltaAlojamientosView.java
+++ src/vistas/AltaAlojamientosView.java
@@ -1,6 +1,5 @@
 package vistas;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
--- src/vistas/AltaClienteView.java
+++ src/vistas/AltaClienteView.java
@@ -26,6 +26,10 @@ import java.awt.event.ActionEvent;
 
 public class AltaClienteView extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField nombreField;
 	private JTextField dniField;
