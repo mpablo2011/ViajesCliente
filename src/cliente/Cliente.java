6b4b2927f5e9d53e6676b98361786d7154af5b88
lucio.tzikas <lucio.tzikas@gmail.com>
2018-09-06 11:51:32 -0300

    ok

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,5 +1,37 @@
 package cliente;
 
-public class Cliente {
+import java.rmi.Naming;
 
+import interfaz.TDAManejoDatos;
+import vistas.MainView;;
+
+public class Cliente {
+	private static TDAManejoDatos manejoDatos;
+
+	public static void main(String[] args) {
+		new Cliente();
+	}
+
+	public boolean getStub() {
+
+		try {
+			manejoDatos = (TDAManejoDatos) Naming
+					.lookup("//127.0.0.1/clasificados");
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public Cliente() {
+		if (getStub()) {
+			MainView menu = new MainView();
+			menu.setVisible(true);
+		}
+	}
+
+	public static TDAManejoDatos getInstancia() {
+		return manejoDatos;
+	}
 }
